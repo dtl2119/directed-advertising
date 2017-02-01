5bf71a53bcbc345c28026658b7e0c03d4aff3cd8
Ubuntu <ubuntu@ip-172-31-0-132.ec2.internal>
2017-02-01 23:52:28 +0000

    Change file name, adDirect.py is now up to date

--- adDirect.py
+++ adDirect.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python
 
-import aws_creds
 from pyspark import SparkContext, SparkConf
 
 # To submit this python application on Spark cluster for execution:
 # $SPARK_HOME/bin/spark-submit <path_to_application> --master spark://<master_public_dns:7077
 
-# sc is a helper value created in the spark-shell, 
-# but not automatically created with spark-submit. 
-# You must instantiate your own SparkContext and use that
+# The helper value sc is created in spark-shell, not automatically
+# created in spark-submit: must instantiate own SparkContext to use
 conf = SparkConf().setAppName("adTarget")
 sc = SparkContext(conf=conf)
 
@@ -16,34 +14,22 @@ sc = SparkContext(conf=conf)
 #file = sc.textFile("hdfs://ec2-34-198-20-105.compute-1.amazonaws.com:9000/user/test.txt")
 
 # For files on disk (file must be on all nodes in the cluster)
-searchRDD = sc.textFile("file:///home/ubuntu/directed-advertising/short_search_batch.txt/")
-purchaseRDD = sc.textFile("file:///home/ubuntu/directed-advertising/short_purchase_batch.txt/")
+webLogRdd = sc.textFile("file:///home/ubuntu/directed-advertising/small_batch_file.txt/")
 
-# Function to parse lines of searches and purchases (CSVs)
-# Timestamp, user_id (int), product_id (int), category_id (int)
-def parseLines(line):
-    time, userid, product, category = line.split(", ")
-    return (int(userid),int(category),int(product))
+# Parse csv: time, user_id, product_id, category_id, action (search or buy)
+parsedRdd = webLogRdd.map(lambda line: line.split(", "))
 
 
-# Parse csv lines in search/purchase RDD: time, user_id, product_id, category_id
-searchRDDParsed = searchRDD.map(lambda line: line.split(", "))
-purchaseRDDParsed = purchaseRDD.map(lambda line: line.split(", "))
+# Aggregate (user_id, category) keys of users who made the purchase (filter out later)
+# Don't advertise to users who searched in a category and made the purchase
+buyKeys = parsedRdd.filter(lambda line: "buy" in line).map(lambda n: (n[1], n[3])).collect()
 
 # Group RDDs --> (user_id, category_id) = [list, of, searches]
-searchGroupRDD = searchRDDParsed.map(lambda n: ((n[1], n[3]), n[2])).groupByKey().map(lambda x: [(x[0]), list(x[1])])
-purchaseGroupRDD = purchaseRDDParsed.map(lambda n: ((n[1], n[3]), n[2])).groupByKey().map(lambda x: [(x[0]), list(x[1])])
+groupedRdd = parsedRdd.map(lambda n: ((n[1], n[3]), n[2])).groupByKey().map(lambda x: [(x[0]), list(x[1])])
 
-# Aggregate (user_id, category) keys in purchase RDD and filter out from search RDD
-# Don't want to advertise to users who searched in a category and made the purchase
-purchaseKeys = purchaseGroupRDD.keys().collect()
-resultSearchRDD = searchGroupRDD.filter(lambda x: x[0] not in purchaseKeys).map(lambda x: [x[0], x[1]])
+# Filter out users who made the purchase in the category they were searching in
+resultRdd = groupedRdd.filter(lambda x: x[0] not in buyKeys).map(lambda x: [x[0], x[1]])
 
-
-result = dict(resultSearchRDD.collect())
-
-# Print key, values of result for testing
-for k,v in result.iteritems():
-    print "%s: %s" % (k, v)
-
+# Dictionary result
+result = dict(resultRdd.collect())
 
