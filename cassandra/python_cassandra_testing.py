aa033377db04a39d18aaac4f437ba35347e2d17f
dtl2119 <dtlimm@gmail.com>
2017-02-03 13:28:24 -0500

    Add python script, basic cassandra querying syntax

---
+++ cassandra/python_cassandra_testing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from cassandra.cluster import Cluster
+
+cluster = Cluster()
+cluster = Cluster(['172.31.0.133']) # Only need 1 private ip, it will auto discover remaining nodes in cluster
+session = cluster.connect()
+session = cluster.connect('advertise') # advertise is the keyspace
+rows = session.execute('SELECT * FROM usersearches')
+
+for r in rows:
+    print r
+
+# Row(userid=u'11111111', categoryid=u'11111111', searches=[u'11111111', u'11111111', u'11111111'])
