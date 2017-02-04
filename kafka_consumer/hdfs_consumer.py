9ddfff8151a264a3c98749526e82ef80ecc65366
dtl2119 <dtlimm@gmail.com>
2017-02-04 13:14:42 -0500

    Add comments and citation to hdfs kafka consumer #1

--- kafka_consumer/hdfs_consumer.py
+++ kafka_consumer/hdfs_consumer.py
@@ -8,36 +8,63 @@ import os
 
 
 class Consumer(object):
+    """Kafka consumer class with functions to consume messages to HDFS.
+
+    Messages blocked into 120MB files and put into HDFS.  Two directories:
+    /user/web_logs: where web activity log files are stored
+    /user/cached: files stored temporarily, overwritten on new batch job
+
+    Attributes:
+        client: IP:port pair of kafka broker
+        consumer: Consumer object specifying the client group/topic
+        temp_file_path: Location of the 120MB file to be appended to before
+            transfer to HDFS
+        temp_file: File object opened from temp_file_path
+        topic: String representing the topic on Kafka
+        group: Represents the Kafka consumer group to be associated with
+        block_cnt: Keep track of block number
+    """
     def __init__(self, addr, group, topic):
-        """Initialize Consumer with kafka broker IP, group, and topic."""
         self.client = KafkaClient(addr)
         self.consumer = SimpleConsumer(self.client, group, topic,
                                        max_buffer_size=1310720000)
         self.temp_file_path = None
         self.temp_file = None
-        self.hadoop_path = "/user/directed-advertising/history"
-        self.cached_path = "/user/directed-advertising/cached"
+        self.hadoop_path = "/user/web_logs"
+        self.cached_path = "/user/cached"
         self.topic = topic
         self.group = group
         self.block_cnt = 0
 
     def consume_topic(self, output_dir):
+        """Consumes a stream of messages from the "messages" topic.
+
+        Code template from https://github.com/ajmssc/bitcoin-inspector.git
+
+        Args:
+            output_dir: Path to store the 120MB block before sending to HDFS
+
+        """
         timestamp = time.strftime('%Y%m%d%H%M%S')
         
         # open file for writing
-        self.temp_file_path = "%s/kafka_%s_%s_%s.dat" % (output_dir,self.topic, self.group, timestamp)
+        self.temp_file_path = "%s/kafka_%s_%s_%s.dat" % (output_dir,
+                                                         self.topic,
+                                                         self.group,
+                                                         timestamp)
         self.temp_file = open(self.temp_file_path,"w")
 
         while True:
             try:
-                # Grab 10000 messages
-                messages = self.consumer.get_messages(count=10000, block=False)
+                # get 1000 messages at a time, non blocking
+                messages = self.consumer.get_messages(count=1000, block=False)
 
-                # Write messages to temp file
+                # OffsetAndMessage(offset=43, message=Message(magic=0,
+                # attributes=0, key=None, value='some message'))
                 for message in messages:
                     self.temp_file.write(message.message.value + "\n")
 
-                # Flush to hdfs once 120MB
+                # file size > 20MB
                 if self.temp_file.tell() > 120000000:
                     self.flush_to_hdfs(output_dir)
 
@@ -49,25 +76,42 @@ class Consumer(object):
 
 
     def flush_to_hdfs(self, output_dir):
-        """
-        Flushes the file into two folders under
-        hdfs://user/directed-advertising/{history, cached}
+        """Flushes the 120MB file to HDFS.
+
+        Code template from https://github.com/ajmssc/bitcoin-inspector.git
+
+        Flushes the file to two folders:
+        hdfs://user/web_logs
+        hdfs://user/cached
+
+        Args:
+            ourput_dir: directory path to store 120MB file before transferring
+                to HDFS
+
         """
         self.temp_file.close()
         timestamp = time.strftime('%Y%m%d%H%M%S')
-        hadoop_fullpath = "%s/%s_%s_%s.dat" % (self.hadoop_path, self.group,self.topic, timestamp)
-        cached_fullpath = "%s/%s_%s_%s.dat" % (self.cached_path, self.group,self.topic, timestamp)
-        print "Block #{} flushing 120MB file to HDFS".format(str(self.block_cnt),hadoop_fullpath)
+        hadoop_fullpath = "%s/%s_%s_%s.dat" % (self.hadoop_path, self.group,
+                                               self.topic, timestamp)
+        cached_fullpath = "%s/%s_%s_%s.dat" % (self.cached_path, self.group,
+                                               self.topic, timestamp)
+        print "Block #{}: Flushing 120MB file to HDFS => {}".format(str(self.block_cnt),
+                                                                  hadoop_fullpath)
         self.block_cnt += 1
 
-        # Write current block to history/cache dirs in hdfs
-        os.system("sudo -u hdfs hdfs dfs -put %s %s" % (self.temp_file_path, hadoop_fullpath))
-        os.system("sudo -u hdfs hdfs dfs -put %s %s" % (self.temp_file_path,cached_fullpath))
+        # place blocked messages into history and cached folders on hdfs
+        os.system("sudo -u hdfs hdfs dfs -put %s %s" % (self.temp_file_path,
+                                                        hadoop_fullpath))
+        os.system("sudo -u hdfs hdfs dfs -put %s %s" % (self.temp_file_path,
+                                                        cached_fullpath))
         os.remove(self.temp_file_path)
 
         timestamp = time.strftime('%Y%m%d%H%M%S')
 
-        self.temp_file_path = "%s/kafka_%s_%s_%s.dat" % (output_dir,self.topic,self.group,timestamp)
+        self.temp_file_path = "%s/kafka_%s_%s_%s.dat" % (output_dir,
+                                                         self.topic,
+                                                         self.group,
+                                                         timestamp)
         self.temp_file = open(self.temp_file_path, "w")
 
 
