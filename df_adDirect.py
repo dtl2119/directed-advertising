327b67c7adcfa3dd553d6ff52f232ff79ab661f7
dtl2119 <dtlimm@gmail.com>
2017-02-08 20:48:23 +0000

    Clean up spark program: make functions, add documentation, create main, rm commented out code #3

--- df_adDirect.py
+++ df_adDirect.py
@@ -2,54 +2,72 @@
 
 from cluster_ips import hdfs
 from pyspark import SparkContext, SparkConf
-#from pyspark.sql import SQLContext
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 
-
-session = SparkSession.builder.appName("adirect").getOrCreate()
+# TO RUN AS SPARK-SUBMIT JOB:
+# spark-submit --packages com.datastax.spark:spark-cassandra-connector_2.11:2.0.0-M3 --conf spark.cassandra.connection.host="ec2-34-199-42-65.compute-1.amazonaws.com" df_adDirect.py
 
 #df = session.read.csv("hdfs://ec2-34-198-20-105.compute-1.amazonaws.com:9000/user/web_logs/small_batch_file.csv")
 def grabFromHDFS(filename):
     hdfs_master = hdfs['master1'] # Piublic IP of NamdeNode
     hdfs_port = "9000"
+    #filename = "*.csv"
     full_hdfs_path = "hdfs://%s:%s/%s/%s" % (hdfs_master, hdfs_port, hdfs['base_dir'], filename)
     #return session.read.csv("hdfs://ec2-34-198-20-105.compute-1.amazonaws.com:9000/user/web_logs/small_batch_file.csv")
     return session.read.csv(full_hdfs_path)
 
-#conf = SparkConf().setAppName("adirect")
-#sc = SparkContext(conf=conf)
-# For files in hdfs
-#file = sc.textFile("hdfs://ec2-34-198-20-105.compute-1.amazonaws.com:9000/user/test.txt")
-#raw_file = session.sparkContext.textFile("hdfs://ec2-34-198-20-105.compute-1.amazonaws.com:9000/user/web_logs/small_batch_file.csv")
-#df = sqlContext.read.format("com.databricks.spark.csv").option("header", "false").load("/home/ubuntu/directed-advertising/small_batch_file.csv") # type: <class 'pyspark.sql.dataframe.DataFrame'>
 
-df = grabFromHDFS('small_batch_file.csv')
+def filterDF(sparksession, df):
+    """
+    This function takes the SparkSession and raw dataframe from HDFS and:
+    - Names the columns, for query readability
+    - Splits raw df of logs into 2: searches and buys
+    """
+    # Rename Columns of DF
+    old_cols = df.schema.names
+    new_cols = ["time", "userid", "productid", "categoryid", "action"]
+    df = reduce(lambda df, i: df.withColumnRenamed(old_cols[i], new_cols[i]), xrange(len(old_cols)), df)
 
-# Rename Columns of DF
-old_cols = df.schema.names
-new_cols = ["time", "userid", "productid", "categoryid", "action"]
-df = reduce(lambda df, i: df.withColumnRenamed(old_cols[i], new_cols[i]), xrange(len(old_cols)), df)
+    # Trim DataFrame to only relevant cols: user, category, product
+    df.createOrReplaceTempView("df_table")
 
-# Trim DataFrame to only relevant cols: user, category, product
-df.createOrReplaceTempView("df_table")
-#df_trimmed = spark.sql("SELECT userid, categoryid, productid FROM df_table")
+    # Get DF of only searches (filter out users who made a purchase)
+    df_searches = session.sql("SELECT userid, categoryid, productid FROM df_table where action = 'search'")
 
-# Get DF of only searches
-df_searches = session.sql("SELECT userid, categoryid, productid FROM df_table where action = 'search'")
+    # Get DF of only buys
+    #df_buys = session.sql("SELECT userid, categoryid, productid FROM df_table where action = 'buy'")
 
-# Get DF of only buys
-df_buys = session.sql("SELECT userid, categoryid, productid FROM df_table where action = 'buy'")
+    # Get in format of: [userid, categoryid]: [list, of, searches] 
+    df_grouped_searches = df_searches.groupby('userid', 'categoryid').agg(F.collect_list('productid').alias('searches'))
 
-df_grouped_searches = df_searches.groupby('userid', 'categoryid').agg(F.collect_list('productid').alias('searches'))
+    return df_grouped_searches
 
-df_grouped_searches.show(3, False)
 
-#session.sql("INSERT INTO advertise.usersearches (userid, categoryid, searches) VALUES ('2', '2', ['2', '2'])")
-#df_grouped_searches.write.format("org.apache.spark.sql.cassandra").mode('append').options(table='usersearches', keyspace='advertise').save()
-#select('userid', 'categoryid', 'searches').saveToCassandra('advertise', 'usersearches')
-df_grouped_searches.write\
-        .format("org.apache.spark.sql.cassandra")\
-        .mode('append')\
-        .options(table="usersearches", keyspace="advertise")\
-        .save()
+def writeToCassandra(df):
+    """
+    Write resulting dataframe to the Cassandra DB
+    """
+    df.write\
+            .format("org.apache.spark.sql.cassandra")\
+            .mode('append')\
+            .options(table="usersearches", keyspace="advertise")\
+            .save()
+
+
+if __name__ == '__main__':
+
+    # Use SparkSession builder to create a new session
+    session = SparkSession.builder.appName("adirect").getOrCreate()
+
+    df = grabFromHDFS('small_batch_file.csv')
+
+    resultDF = filterDF(session, df)
+
+    # FIXME: For testing
+    resultDF.show(3, False)
+
+    writeToCassandra(resultDF)
+
+    # Stop the underlying SparkContext
+    session.stop()
