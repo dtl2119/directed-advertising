b2947854734fb7c8c5f147d4829f55e64d589178
dtl2119 <dtlimm@gmail.com>
2017-02-15 08:23:13 +0000

    Add documentation to hdfs kafka consumer #1

--- kafka/consumer/hdfs_consumer.py
+++ kafka/consumer/hdfs_consumer.py
@@ -9,21 +9,26 @@ import os
 import subprocess
 
 
+# Code template from https://github.com/ajmssc/bitcoin-inspector.git
+
 class Consumer(object):
-    """Kafka consumer class with functions to consume messages to HDFS.
+    """
+    Kafka Consumer class: consumes messages from a topic, and pushes to HDFS
 
     Messages blocked into 120MB files and put into HDFS.  Two directories:
     /user/web_logs: where web activity log files are stored
-    /user/cached: files stored temporarily, overwritten on new batch job
+    /user/cached: files stored temporarily, overwritten on new batch job # FIXME
 
     Attributes:
         client: IP:port pair of kafka broker
         consumer: Consumer object specifying the client group/topic
-        temp_file_path: Location of the 120MB file to be appended to before
+        temp_file_path: Local on-disk path for temp_file, where  messages are accumulated
             transfer to HDFS
-        temp_file: File object opened from temp_file_path
-        topic: String representing the topic on Kafka
-        group: Represents the Kafka consumer group to be associated with
+        temp_file: Actual temporary file where messages are  written locally 
+            on disk until it reaches 120MB, at which point it's flushed to
+            hdfs and then overwritten
+        topic: Topic name the Consumer is subscribing to
+        group: Kafka consumer group to be associated with
         block_cnt: Keep track of block number
     """
     def __init__(self, addr, group, topic):
@@ -33,39 +38,36 @@ class Consumer(object):
         self.temp_file_path = None
         self.temp_file = None
         self.hadoop_path = "/user/web_logs"
-        self.cached_path = "/user/cached"
+        #self.cached_path = "/user/cached" # FIXME
         self.topic = topic
         self.group = group
         self.block_cnt = 0
 
     def consume_topic(self, output_dir):
-        """Consumes a stream of messages from the "messages" topic.
+        """
+        Consumes stream of messages from specified topic (in main)
 
-        Code template from https://github.com/ajmssc/bitcoin-inspector.git
-
-        Args:
-            output_dir: Path to store the 120MB block before sending to HDFS
-
+        output_dir: Path where 120MB temp_file is, write to file before 
+           flushing to HDFS
         """
         timestamp = time.strftime('%Y%m%d%H%M%S')
         
-        # open file for writing
-        self.temp_file_path = "%s/kafka_%s_%s_%s.csv" % (output_dir,self.topic,self.group,timestamp)
+        # Write file locally (temporary)
+        self.temp_file_path = "%s/kafka_%s_%s_%s.csv" % (output_dir,self.topic, self.group, timestamp)
         self.temp_file = open(self.temp_file_path,"w")
 
         while True:
             try:
                 # get 1000 messages at a time, non blocking
-                messages = self.consumer.get_messages(count=1000, block=False)
+                messages = self.consumer.get_messages(count=5000, block=False)
 
-                # OffsetAndMessage(offset=43, message=Message(magic=0,
-                # attributes=0, key=None, value='some message'))
+                # OffsetAndMessage(offset=43, message=Message(magic=0, # FIXME
+                # attributes=0, key=None, value='some message')) # FIXME
                 for message in messages:
                     self.temp_file.write(message.message.value + "\n")
 
                 # file size > 120MB
-                #if self.temp_file.tell() > 120000000:
-                if self.temp_file.tell() > 60000: # FIXME
+                if self.temp_file.tell() > 120000000:
                     self.flush_to_hdfs(output_dir)
 
                 self.consumer.commit()
@@ -76,23 +78,21 @@ class Consumer(object):
 
 
     def flush_to_hdfs(self, output_dir):
-        """Flushes the 120MB file to HDFS.
-
-        Code template from https://github.com/ajmssc/bitcoin-inspector.git
+        """
+        Flush 120MB file 
 
         Flushes the file to two folders:
         hdfs://user/web_logs
-        hdfs://user/cached
+        hdfs://user/cached #FIXME
 
-        Args:
-            ourput_dir: directory path to store 120MB file before transferring
+        output_dir: Path where  120MB file before transferring
                 to HDFS
 
         """
         self.temp_file.close()
         timestamp = time.strftime('%Y%m%d%H%M%S')
-        hadoop_fullpath = "%s/%s_%s_%s.csv" % (self.hadoop_path, self.group,self.topic, timestamp)
-        cached_fullpath = "%s/%s_%s_%s.csv" % (self.cached_path, self.group,self.topic, timestamp)
+        hadoop_fullpath = "%s/%s_%s_%s.csv" % (self.hadoop_path, self.topic, timestamp) # FIXME NOW
+        #cached_fullpath = "%s/%s_%s_%s.csv" % (self.cached_path, self.group,self.topic, timestamp)
 
         print "Block #{}: Flushing 120MB file to HDFS => {}".format(str(self.block_cnt),hadoop_fullpath)
         self.block_cnt += 1
@@ -101,13 +101,13 @@ class Consumer(object):
 #        os.system("sudo -u ubuntu hdfs dfs -put %s %s" % (self.temp_file_path,hadoop_fullpath)) # FIXME
 #        os.system("sudo -u ubuntu hdfs dfs -put %s %s" % (self.temp_file_path,cached_fullpath)) # FIXME
         os.system("hdfs dfs -put %s %s" % (self.temp_file_path,hadoop_fullpath))
-        os.system("hdfs dfs -put %s %s" % (self.temp_file_path,cached_fullpath))
+        #os.system("hdfs dfs -put %s %s" % (self.temp_file_path,cached_fullpath)) # FIXME
 
         os.remove(self.temp_file_path)
 
         timestamp = time.strftime('%Y%m%d%H%M%S')
 
-        self.temp_file_path = "%s/kafka_%s_%s_%s.csv" % (output_dir,self.topic,self.group,timestamp)
+        self.temp_file_path = "%s/kafka_%s_%s_%s.csv" % (output_dir,self.topic, timestamp) #FIXME NOW (btwn topic and timestamp
 
         self.temp_file = open(self.temp_file_path, "w")
 
@@ -115,6 +115,7 @@ class Consumer(object):
 if __name__ == '__main__':
 
     print "Consuming web logs (topic: web_activity1): . . ."
-    cons = Consumer(addr="localhost:9092", group="hdfs", topic="web_activity1")
-    cons.consume_topic("/home/ubuntu/directed-advertising/kafka_consumer/kafka_msgs")
+    kafka_msgs_path = "%s/kafka_msgs" % (os.path.dirname(os.path.realpath(__file__)))
+    cons = Consumer(addr="localhost:9092", group = "group",  topic="web_activity1")
+    cons.consume_topic(kafka_msgs_path)
 
